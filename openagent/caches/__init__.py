84a9af53fa3e6c433111db4a6da0b65728258e7c
Anurag <25473195+anubrag@users.noreply.github.com>
2023-09-08 22:44:09 +0530

    remove unnecessary dependencies

--- openagent/caches/__init__.py
+++ openagent/caches/__init__.py
@@ -1,5 +1,3 @@
 from ._cache import BaseCache
 from ._diskcache import DiskCache
 from ._gptcache import GPTCache
-from ._chromacache import ChromaSemanticCache
-from ._memorycache import LocalMemoryCache
