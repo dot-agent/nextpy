1b632d43d9e6ec04f44d1ce12de65719b8ecdc53
anubrag <b.anurag@aol.com>
2023-12-25 17:11:12 +0530

    updated code for blank

--- nextpy/frontend/templates/apps/blank/code/blank.py
+++ nextpy/frontend/templates/apps/blank/code/blank.py
@@ -2,31 +2,36 @@ from xtconfig import config
 
 import nextpy as xt
 
-# Style dictionary 
+# This code block represents a style dictionary generated using a large language model (LLM). 
+# The original prompt for this generation was: "Generate a style dictionary for the following code [paste the code here]".
+# Additionally, we have developed the "Nextpy Style Assistant", a tool designed to assist in code styling and best practices. 
+# This assistant will be available on our Discord server soon for further support and guidance in code styling.
+
 style = {
     "heading": {
         "text_align": "center",
-        "font_size": "28px",  # Fixed font size
-        "line_height": "36px", 
+        "font_size": "28px",
+        "line_height": "36px",
+        "color": "#F3F5F7",
         "font_family": "DM Sans",
-        "max_width": "620px",  # Fixed maximum width
+        "max_width": "620px",
         "padding_top": "16px",
-        "padding_bottom": "16px", 
+        "padding_bottom": "16px",
     },
     "get_started_box": {
         "font_family": "Inter",
         "text_align": "center",
-        "font_size": "14px",  # Fixed font size
+        "font_size": "14px",
         "color": "black",
         "background": "white",
-        "padding_x": "48px",  
-        "padding_y": "12px",  # Fixed vertical padding
+        "padding_x": "48px",
+        "padding_y": "12px",
         "border_radius": "lg",
     },
     "vertical_stack_style": {
         "background": "#04090B",
-        "height": "100vh",  # Using viewport height unit for full height
-        "padding_top": "20%",  # Percentage-based padding for dynamic vertical spacing
+        "height": "100vh",
+        "padding_top": "20%",
         "padding_left":"5%",
         "padding_right":"5%",
 
@@ -52,7 +57,6 @@ def index() -> xt.Component:
                 xt.image(src="/gradient_underline.svg", width="150px"),
                 **style["get_started_box"]
             ),
-            
             **style["vertical_stack_style"]
         ),
     )
