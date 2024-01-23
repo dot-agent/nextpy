93bef3c9cfa99a0a17242c81787977f534b7ae3f
Raj <84141638+Raj4646@users.noreply.github.com>
2024-01-23 17:49:00 +0530

    Code refactor - Gridx Portfolio (#126)
    * improved code readability and responsive fixes
    
    * gridx code readability improvements

---
+++ app-examples/gridx_portfolio/gridx_portfolio/components/navbar.py
@@ -0,0 +1,46 @@
+import nextpy as xt
+
+
+class NavbarState(xt.State):
+    links = [
+        "Home",
+        "About",
+        "Works",
+        "Contact",
+    ]
+
+
+def Navbar():
+    return xt.box(
+        xt.hstack(
+            xt.image(src="logo.svg", width="90px"),
+            xt.desktop_only(
+                xt.hstack(
+                    xt.foreach(
+                        NavbarState.links,
+                        lambda links: xt.link(
+                            xt.text(links), src="#", _hover={"text_decoration": "none"}
+                        ),
+                    ),
+                    class_name="hidden md:flex gap-6 md:items-center md:space-x-6 text-white text-base ",
+                    spacing="1.5rem",
+                ),
+            ),
+            xt.button(
+                xt.center(
+                    "Lets Talk",
+                    bg="#323232",
+                    py="12px",
+                    px="2rem",
+                    class_name="hover:bg-white text-white hover:text-[#323232] text-sm font-semibold transition ease-in-out rounded-2xl",
+                ),
+                variant="unstyled",
+                color_scheme="none",
+            ),
+            justify_content="space-between",
+            align_items="center",
+        ),
+        class_name="bg-[#0F0F0F] py-6 px-4 lg:px-48",
+        width="100%",
+        z_index="5",
+    )
