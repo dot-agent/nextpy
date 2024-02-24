732e087d42ff218b25f3a9bc49f657a1cd6a8b08
shravya-34 <shettyshravya34@gmail.com>
2024-02-24 18:37:41 +0530

    typehint for color_picker changed

--- nextpy/interfaces/web/components/chakra/forms/color_picker.py
+++ nextpy/interfaces/web/components/chakra/forms/color_picker.py
@@ -1,13 +1,13 @@
 from nextpy.interfaces.web.components.component import Component
 # from nextpy import Var 
-from typing import Any
+from typing import Dict, Any
 
 class ColorPicker(Component):
     library = "react-colorful"
     tag = "HexColorPicker"
     color: str = None
 
-    def get_event_triggers(self) -> dict[str, Any]:
+    def get_event_triggers(self) -> Dict[str, Any]:
         return {
             **super().get_event_triggers(),
             "on_change": lambda e0: [e0],
